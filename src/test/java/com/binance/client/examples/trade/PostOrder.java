619cb35b222eaf0746159119ba66db3b472e52d5
cy <791508825@qq.com>
2022-06-16 20:38:34 +0800

    test

--- src/test/java/com/binance/client/examples/trade/PostOrder.java
+++ src/test/java/com/binance/client/examples/trade/PostOrder.java
@@ -20,7 +20,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class PostOrder {
-    public static void sendmsg() {
+    public static void sendmsg(String message) {
         try {
             JSONObject postInfo = new JSONObject();
             postInfo.put("msgtype", "markdown");
@@ -28,7 +28,7 @@ public class PostOrder {
             // 第一屏也就是钉钉消息列表里面的消息
             String title = "", context = "OKOK,小钱到手test";
             markdown.put("title", title + "|运维");
-            markdown.put("text", context);
+            markdown.put("text", message);
             postInfo.put("markdown", markdown);
             postInfo.put("isAtAll", false);
             Long timestamp = System.currentTimeMillis();
@@ -62,79 +62,107 @@ public class PostOrder {
     public static void main(String[] args) {
         String symbol = "ETHUSDT";
         String symbol1 = "SOLUSDT";
-        int symbolScale = 4;
-        int symbol1Scale = 2;
-        BigDecimal value = BigDecimal.valueOf(500);
 
         RequestOptions options = new RequestOptions();
         SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
                 options);
-
-        List<SymbolPrice> symbolPrices = syncRequestClient.getSymbolPriceTicker(symbol);
-        List<SymbolPrice> symbol1Prices = syncRequestClient.getSymbolPriceTicker(symbol1);
-
-        if (symbolPrices == null || symbolPrices.size() == 0 || symbol1Prices == null || symbol1Prices.size() == 0
-                || System.currentTimeMillis() - symbolPrices.get(0).getTime() > 2000
-                || System.currentTimeMillis() - symbol1Prices.get(0).getTime() > 2000) {
-            System.out.println("无有效价格");
-            return;
-        }
-
-        if (RandomUtils.nextBoolean()) {
-            System.out.println(syncRequestClient.postOrder(symbol,
-                    OrderSide.BUY, PositionSide.BOTH, OrderType.MARKET, null,
-                    value.divide(symbolPrices.get(0).getPrice(), symbolScale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
-            System.out.println(syncRequestClient.postOrder(symbol1,
-                    OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
-                    value.divide(symbol1Prices.get(0).getPrice(), symbol1Scale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
-        } else {
-            System.out.println(syncRequestClient.postOrder(symbol,
-                    OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
-                    value.divide(symbolPrices.get(0).getPrice(), symbolScale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
-            System.out.println(syncRequestClient.postOrder(symbol1,
-                    OrderSide.BUY, PositionSide.BOTH, OrderType.MARKET, null,
-                    value.divide(symbol1Prices.get(0).getPrice(), symbol1Scale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
-        }
-
-
         while (true) {
+            //先检查持仓
+            List<PositionRisk> risks1 = null;
             try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            List<PositionRisk> risks = null;
-            try {
-                risks = syncRequestClient.getPositionRisk();
+                risks1 = syncRequestClient.getPositionRisk();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            if (risks == null || risks.size() == 0) {
+            if (risks1 == null || risks1.size() == 0) {
                 System.out.println("获取持仓为空");
                 continue;
             }
 
-            Map<String, PositionRisk> positionMap = risks.stream().filter(item -> "BOTH".equals(item.getPositionSide())).collect(Collectors.toMap(PositionRisk::getSymbol, Function.identity()));
-            PositionRisk positionRisk = positionMap.get(symbol);
-            PositionRisk positionRisk1 = positionMap.get(symbol1);
+            Map<String, PositionRisk> positionMap1 = risks1.stream().filter(item -> "BOTH".equals(item.getPositionSide())).collect(Collectors.toMap(PositionRisk::getSymbol, Function.identity()));
+            PositionRisk firstCheck = positionMap1.get(symbol);
+            PositionRisk firstCheck1 = positionMap1.get(symbol1);
 
-            if (positionRisk.getPositionAmt().compareTo(BigDecimal.ZERO) == 0 || positionRisk1.getPositionAmt().compareTo(BigDecimal.ZERO) == 0) {
-                System.out.println("有一边持仓为空,请检查");
+            if (firstCheck.getPositionAmt().compareTo(BigDecimal.ZERO) != 0 || firstCheck1.getPositionAmt().compareTo(BigDecimal.ZERO) != 0) {
+                sendmsg("出现异常,请检查");
                 break;
             }
-            BigDecimal condition = positionRisk.getUnrealizedProfit().add(positionRisk1.getUnrealizedProfit());
-            if (condition.compareTo(BigDecimal.valueOf(13)) >= 0) {
+
+            int symbolScale = 3;
+            int symbol1Scale = 0;
+            BigDecimal value = BigDecimal.valueOf(500);
+
+
+            List<SymbolPrice> symbolPrices = syncRequestClient.getSymbolPriceTicker(symbol);
+            List<SymbolPrice> symbol1Prices = syncRequestClient.getSymbolPriceTicker(symbol1);
+
+            if (symbolPrices == null || symbolPrices.size() == 0 || symbol1Prices == null || symbol1Prices.size() == 0
+                    || System.currentTimeMillis() - symbolPrices.get(0).getTime() > 2000
+                    || System.currentTimeMillis() - symbol1Prices.get(0).getTime() > 2000) {
+                System.out.println("无有效价格");
+                continue;
+            }
+            try {
+
+                if (RandomUtils.nextBoolean()) {
+                    System.out.println(syncRequestClient.postOrder(symbol,
+                            OrderSide.BUY, PositionSide.BOTH, OrderType.MARKET, null,
+                            value.divide(symbolPrices.get(0).getPrice(), symbolScale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
+                    System.out.println(syncRequestClient.postOrder(symbol1,
+                            OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
+                            value.divide(symbol1Prices.get(0).getPrice(), symbol1Scale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
+                } else {
+                    System.out.println(syncRequestClient.postOrder(symbol,
+                            OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
+                            value.divide(symbolPrices.get(0).getPrice(), symbolScale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
+                    System.out.println(syncRequestClient.postOrder(symbol1,
+                            OrderSide.BUY, PositionSide.BOTH, OrderType.MARKET, null,
+                            value.divide(symbol1Prices.get(0).getPrice(), symbol1Scale, RoundingMode.HALF_DOWN).toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            while (true) {
                 try {
-                    System.out.println(syncRequestClient.postOrder(positionRisk.getSymbol(),
-                            positionRisk.getPositionAmt().compareTo(BigDecimal.ZERO) < 0 ? OrderSide.BUY : OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
-                            positionRisk.getPositionAmt().abs().toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
-                    System.out.println(syncRequestClient.postOrder(symbol1, positionRisk1.getPositionAmt().compareTo(BigDecimal.ZERO) < 0 ? OrderSide.BUY : OrderSide.SELL,
-                            PositionSide.BOTH, OrderType.MARKET, null,
-                            positionRisk1.getPositionAmt().abs().toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
-                    sendmsg();
-                    break;
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                List<PositionRisk> risks = null;
+                try {
+                    risks = syncRequestClient.getPositionRisk();
                 } catch (Exception e) {
                     e.printStackTrace();
+                }
+                if (risks == null || risks.size() == 0) {
+                    System.out.println("获取持仓为空");
+                    continue;
+                }
+
+                Map<String, PositionRisk> positionMap = risks.stream().filter(item -> "BOTH".equals(item.getPositionSide())).collect(Collectors.toMap(PositionRisk::getSymbol, Function.identity()));
+                PositionRisk positionRisk = positionMap.get(symbol);
+                PositionRisk positionRisk1 = positionMap.get(symbol1);
+
+                if (positionRisk.getPositionAmt().compareTo(BigDecimal.ZERO) == 0 || positionRisk1.getPositionAmt().compareTo(BigDecimal.ZERO) == 0) {
+                    System.out.println("有一边持仓为空,请检查");
+                    break;
+                }
+                BigDecimal condition = positionRisk.getUnrealizedProfit().add(positionRisk1.getUnrealizedProfit());
+                if (condition.compareTo(BigDecimal.valueOf(13)) >= 0) {
+                    try {
+                        System.out.println(syncRequestClient.postOrder(positionRisk.getSymbol(),
+                                positionRisk.getPositionAmt().compareTo(BigDecimal.ZERO) < 0 ? OrderSide.BUY : OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
+                                positionRisk.getPositionAmt().abs().toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
+                        System.out.println(syncRequestClient.postOrder(symbol1, positionRisk1.getPositionAmt().compareTo(BigDecimal.ZERO) < 0 ? OrderSide.BUY : OrderSide.SELL,
+                                PositionSide.BOTH, OrderType.MARKET, null,
+                                positionRisk1.getPositionAmt().abs().toPlainString(), null, null, null, null, null, NewOrderRespType.RESULT));
+                        sendmsg("OKOK,小钱到手test");
+                        break;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -143,4 +171,6 @@ public class PostOrder {
 //        System.out.println(syncRequestClient.postOrder("BTCUSDT", OrderSide.SELL, PositionSide.BOTH, OrderType.MARKET, null,
 //                null, null, null, null, null, null, NewOrderRespType.RESULT));
     }
+
+
 }
